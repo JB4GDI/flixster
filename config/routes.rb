2ed7ec495153dce302f096cc606dadfa77c99925
Jaime Barriga <JB4GDI@gmail.com>
2017-10-11 16:40:21 +0000

    Added a course detail page and connected lessons properly

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
 
     # This line is by design: By specifying only: [], we specify that we want to only hook up URLs for [], the empty array, or in other words not to add anything.
