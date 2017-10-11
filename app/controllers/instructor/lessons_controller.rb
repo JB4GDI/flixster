6db6f203ed76973e6b4b44fb71d1e3125be426bc
Jaime Barriga <JB4GDI@gmail.com>
2017-10-11 01:18:03 +0000

    Lessons are now secured and we cleaned up the repeated code into helper functions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,8 +1,11 @@
 class Instructor::LessonsController < ApplicationController
 
+  # Make sure the user is logged in first
+  before_action :authenticate_user!
+
+  before_action :require_authorized_for_current_section
+
   def new
-    # We need to pull in the section_id from the parameters
-    @section = Section.find(params[:section_id])
 
     # Build a blank lesson to flesh out
     @lesson = Lesson.new
@@ -11,18 +14,31 @@ class Instructor::LessonsController < ApplicationController
 
   def create
 
-    # Look up the section in the DB that this lesson is for
-    @section = Section.find(params[:section_id])
-
     # Create the lesson in the DB and connect it to the section we looked up
-    @lesson = @section.lessons.create(lesson_params)
+    @lesson = current_section.lessons.create(lesson_params)
 
     # Redirect to the instructor dashboard
-    redirect_to instructor_course_path(@section.course)
+    redirect_to instructor_course_path(current_section.course)
 
   end
 
   private
+
+  def require_authorized_for_current_section
+
+    # The user needs to own this lesson to work with it
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+    
+  end
+
+  helper_method :current_section
+  def current_section
+    # The ||= means to not look it up if we've found a value already.  It's 'memoization'
+    # We need to pull in the section_id from the parameters
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
