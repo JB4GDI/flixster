80469a6509f3b6f8b41668cc19cb4517e6821d19
Jaime Barriga <JB4GDI@gmail.com>
2017-11-13 18:35:38 +0000

    Built the enrollment functionality and display the enrollment button/status to students

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+
+    # Collect automatically builds an array to store these enrollment items
+    # enrolled_courses = enrollments.collect do |enrollment|
+    #   enrollment.course
+    # end
+
+    # And this is the shorthand of the code above
+    # enrolled_courses = enrollments.collect(&:course)
+
+    # But ultimately, we're going to do all the work in the has_many part at the top
+    return enrolled_courses.include?(course)
+
+  end
+
 end
